c8d9ef146bb7d4fc90f191ab197f9ef08d4dbbc7
Jeddi212 <jedediahf51@gmail.com>
2023-04-04 18:16:45 +0700

    🧑‍🎄 :: inverse & it spec

--- spec/local_search_spec.cr
+++ spec/local_search_spec.cr
@@ -6,4 +6,10 @@ describe "Local Search" do
 			insertion([1, 2, 3, 4, 5, 6, 7], 1, 4).should eq([1, 5, 2, 3, 4, 6, 7])
 		end
 	end
+
+	describe "#inverse" do
+		it "should works" do
+			inverse([1, 2, 3, 4, 5, 6, 7], 1, 4).should eq([1, 5, 4, 3, 2, 6, 7])
+		end
+	end
 end
--- src/method/extra/local_search.cr
+++ src/method/extra/local_search.cr
@@ -2,3 +2,8 @@ def insertion(r, i, j)
 	r[i+1..j], r[i] = r[i...j], r[j]
 	return r
 end
+
+def inverse(r, i, j)
+	r[i..j] = r[i..j].reverse
+	return r
+end
